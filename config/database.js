e99b8243a98ae3fceca96efc6b2df4a1614d48f7
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-10-24 13:31:29 -0400

    sun

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const username = "UrjiH";
+const password = "getpassword!";
+const dbName = "roulette";
+
+module.exports = {
+  url: `    // "mongodb+srv://UrjiH:getpassword@cluster0.rh8ci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    `,
+};
+
+// // config/database.js
+// const username  = 'Mary'
+// const password  = 'BZxn35BzacSsb.!'
+// const dbName    = 'roulette'
+
+// module.exports = {
+//     'url' : `mongodb+srv://${username}:${password}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
+// //mongodb+srv://Mary:<password>@taskmanagercluster.aqyy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
